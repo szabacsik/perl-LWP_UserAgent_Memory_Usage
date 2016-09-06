59908a5f0aad38a483c73f5402c3b893bda6fd7e
András Szabácsik <packman@freemail.hu>
2016-09-06 17:53:20 +0200

    Backup

--- LWP_UserAgent_Memory_Usage.pl
+++ LWP_UserAgent_Memory_Usage.pl
@@ -14,7 +14,7 @@ local $| = 1;
 my $i;
 
 query (); #once
-my $mu = Memory::Usage -> new ();
+my $mu = Memory::Usage -> new ( keep_alive => 0 );
 $mu -> record ( 'started' );
 
 #main loop
@@ -33,17 +33,17 @@ $mu -> dump ();
 #http query
 sub query
 {
-	my $user_agent = LWP::UserAgent -> new(ssl_opts => { verify_hostname => 0 });
+	my $user_agent = LWP::UserAgent -> new;
 	$user_agent -> timeout ( 60 );
 	$user_agent -> agent ( "NXLog" );
-	my $headers = HTTP::Headers -> new ();
+	#my $headers = HTTP::Headers -> new ();
 	my $http_request;
 	my $http_response;
 	my $url = 'http://localhost/';
 	my $content = '';
-	$headers -> clear ();
-	$headers -> header ( 'Authorization', "Bearer " . "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IlliUkFRUlljRV9tb3RXVkpLSHJ3TEJiZF85cyIsImtpZCI6IlliUkFRUlljRV9tb3RXVkpLSHJ3TEJiZF85cyJ9.eyJhdWQiOiJodHRwczovL21hbmFnZS5vZmZpY2UuY29tIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvZTY4MWI0OTMtMTRhOC00MzhiLThiYmYtZDY1YWJkYzgyNmMyLyIsImlhdCI6MTQ3MzE2MzM2MywibmJmIjoxNDczMTYzMzYzLCJleHAiOjE0NzMxNjcyNjMsImFwcGlkIjoiOTEyNDk3YmEtOTc4MC00NmJjLWE2YTYtM2E1NmE0YzE0Mjc4IiwiYXBwaWRhY3IiOiIyIiwiaWRwIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvZTY4MWI0OTMtMTRhOC00MzhiLThiYmYtZDY1YWJkYzgyNmMyLyIsIm9pZCI6ImVhNmY4MGRkLTdhNGQtNDRjMS04NGE4LTZlYTdiNmViYjQzYSIsInJvbGVzIjpbIlRocmVhdEludGVsbGlnZW5jZS5SZWFkIiwiQWN0aXZpdHlSZXBvcnRzLlJlYWQiLCJUaHJlYXRJbnRlbGxpZ2VuY2UuUmVhZCIsIkFjdGl2aXR5RmVlZC5SZWFkRGxwIiwiQWN0aXZpdHlSZXBvcnRzLlJlYWQiLCJTZXJ2aWNlSGVhbHRoLlJlYWQiLCJBY3Rpdml0eUZlZWQuUmVhZCJdLCJzdWIiOiJlYTZmODBkZC03YTRkLTQ0YzEtODRhOC02ZWE3YjZlYmI0M2EiLCJ0aWQiOiJlNjgxYjQ5My0xNGE4LTQzOGItOGJiZi1kNjVhYmRjODI2YzIiLCJ2ZXIiOiIxLjAifQ.jWDuVjssjkrRV4Of2sMT4Imny0W5jMuj8NisX2v-ji45WzJvaEnSMHGevMOGbIwT6L1K9sSGGVeIytgwm5EA3NnFnYpJ9syHF_iPUamM0zQ2XAN3Bsa5ep7WzaD8cE3al6_Nl7fiwchHsN5mXcs5s9JZbafwjSknfsffbXCmWxli9jZQmViKRthxOH87C588Fc0vdNuubkJyCQ_3P_ssMRuCJYarwgAfhkoAF4855BbZKy3bKTMLwwu8072NDI23yTzi1T9QKttXsW_Jaf70931atZ5vueE2wnD1r0aa01o7EzCuxsBi7EJBU0ywg8lestFX720bthVrQdAXHNAQgQ" );
-	$http_request = HTTP::Request -> new ( 'GET', $url, $headers );
+	#$headers -> clear ();
+	#$headers -> header ( 'Authorization', "Bearer " . "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IlliUkFRUlljRV9tb3RXVkpLSHJ3TEJiZF85cyIsImtpZCI6IlliUkFRUlljRV9tb3RXVkpLSHJ3TEJiZF85cyJ9.eyJhdWQiOiJodHRwczovL21hbmFnZS5vZmZpY2UuY29tIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvZTY4MWI0OTMtMTRhOC00MzhiLThiYmYtZDY1YWJkYzgyNmMyLyIsImlhdCI6MTQ3MzE2MzM2MywibmJmIjoxNDczMTYzMzYzLCJleHAiOjE0NzMxNjcyNjMsImFwcGlkIjoiOTEyNDk3YmEtOTc4MC00NmJjLWE2YTYtM2E1NmE0YzE0Mjc4IiwiYXBwaWRhY3IiOiIyIiwiaWRwIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvZTY4MWI0OTMtMTRhOC00MzhiLThiYmYtZDY1YWJkYzgyNmMyLyIsIm9pZCI6ImVhNmY4MGRkLTdhNGQtNDRjMS04NGE4LTZlYTdiNmViYjQzYSIsInJvbGVzIjpbIlRocmVhdEludGVsbGlnZW5jZS5SZWFkIiwiQWN0aXZpdHlSZXBvcnRzLlJlYWQiLCJUaHJlYXRJbnRlbGxpZ2VuY2UuUmVhZCIsIkFjdGl2aXR5RmVlZC5SZWFkRGxwIiwiQWN0aXZpdHlSZXBvcnRzLlJlYWQiLCJTZXJ2aWNlSGVhbHRoLlJlYWQiLCJBY3Rpdml0eUZlZWQuUmVhZCJdLCJzdWIiOiJlYTZmODBkZC03YTRkLTQ0YzEtODRhOC02ZWE3YjZlYmI0M2EiLCJ0aWQiOiJlNjgxYjQ5My0xNGE4LTQzOGItOGJiZi1kNjVhYmRjODI2YzIiLCJ2ZXIiOiIxLjAifQ.jWDuVjssjkrRV4Of2sMT4Imny0W5jMuj8NisX2v-ji45WzJvaEnSMHGevMOGbIwT6L1K9sSGGVeIytgwm5EA3NnFnYpJ9syHF_iPUamM0zQ2XAN3Bsa5ep7WzaD8cE3al6_Nl7fiwchHsN5mXcs5s9JZbafwjSknfsffbXCmWxli9jZQmViKRthxOH87C588Fc0vdNuubkJyCQ_3P_ssMRuCJYarwgAfhkoAF4855BbZKy3bKTMLwwu8072NDI23yTzi1T9QKttXsW_Jaf70931atZ5vueE2wnD1r0aa01o7EzCuxsBi7EJBU0ywg8lestFX720bthVrQdAXHNAQgQ" );
+	$http_request = HTTP::Request -> new ( 'GET', $url ); #, $headers );
 	$http_response = $user_agent -> request ( $http_request );
 	if ( $http_response -> is_success )
     {
